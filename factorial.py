f6886c34a08dafce8d9a8a07fdddaba820a671ce
Nalugya31 <136578468+Nalugya31@users.noreply.github.com>
2023-10-16 07:37:42 +0300

    This is my file for already written code

---
+++ factorial.py
@@ -0,0 +1,8 @@
+# factorial.py
+def calculate_factorial(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
+    if n == 0:
+        return 1
+    else:
+        return n * calculate_factorial(n - 1)
