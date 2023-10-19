376f2c8e5404d4f794c3d121c8d7c36af51cfeb8
Nalugya31 <136578468+Nalugya31@users.noreply.github.com>
2023-10-19 07:56:38 +0300

    This is my file for tax.py tests

--- test_factorial.py
+++ test_factorial.py
@@ -2,17 +2,17 @@ from factorial import calculate_factorial
 import pytest
 
 
-def test_calculate_factorial_0f_zero():
-    assert calculate_factorial(0) == 1
+# def test_calculate_factorial_0f_zero():
+#     assert calculate_factorial(0) == 1
 
 
-def test_calculate_factorial_of_positive_value():
-    assert calculate_factorial(5) == 120
+# def test_calculate_factorial_of_positive_value():
+#     assert calculate_factorial(5) == 120
 
 
-def test_calculate_factorial_of_negative_value():
-    with pytest.raises(ValueError):
-        calculate_factorial(-5)
+# def test_calculate_factorial_of_negative_value():
+#     with pytest.raises(ValueError):
+#         calculate_factorial(-5)
 
 
 
